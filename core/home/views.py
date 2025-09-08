f5d897454df6be9b6728a912622ee1be038d5222
kritiadhikari135 <kriteeadhikari90@gmail.com>
2025-09-08 19:38:02 +0545

    template engine

--- core/home/views.py
+++ core/home/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request , "index.html")
+    people = [
+        { 'name': 'ram', 'age': 26},
+        {'name': 'shyam', 'age': 17},
+        {'name': 'z', 'age': 64},
+        {'name': 'sandeep', 'age': 18},
+    ]
+    
+
+    return render(request , "home/index.html", context = {'peoples': people});
